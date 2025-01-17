25928e395d81ffe5e994446bc6a45bd660e15299
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2025-01-18 00:37:46 +0100

    Resolve absolute paths for frontend build
    Resolve absolute paths for frontend build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 });
